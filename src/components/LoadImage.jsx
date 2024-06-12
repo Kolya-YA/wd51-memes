87a5fac732ecee823ad19f55e72fe82d1d6d83b9
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-06-12 16:55:22 +0200

    add input for image upload

--- src/components/LoadImage.jsx
+++ src/components/LoadImage.jsx
@@ -1,9 +1,22 @@
-const LoadImage = () => {
+const LoadImage = ({ handleNewImage }) => {
 
     return (
         <div>
             <h2 className="mb-4 text-2xl font-semibold">Load image</h2>
             <p>Not ready. Stay tuned.</p>
+            <label className="my-4 block">Load image for new meme (jpg, png): 
+                <input
+                    type="file" name="newImgMeme"
+                    accept=".jpg, .jpeg, .png"
+                    onChange={handleNewImage}
+                    className="
+                    block w-full mt-2 p-2 border border-gray-300 rounded-md
+                    text-sm text-stone-500
+                    file:mr-5 file:py-1 file:px-2 file:border file:bg-white 
+                    file:rounded-md file:cursor-pointer
+                    "
+                />
+            </label>
         </div>
     )
 }
